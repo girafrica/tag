39bb8e5a51f1c6ed9836e38a1e3af35fbfe6af05
Alexander Davydov <Alexander_Davydov@epam.com>
2024-03-01 11:48:44 +0300

    3

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -10,21 +10,7 @@ pipeline {
 
     
     stages {
-        stage('Check changes') {
-            steps {
-                script {
-                    dir ("changes"){
-                        sh (' ls -l ')
-                        cloneToLocation('https://github.com/girafrica/test1.git', 'github-app', 'main', '.')
-                        //sh (' git fetch https://github.com/girafrica/test1.git ')
-                        change = sh(returnStdout: true, script: 'git log $(git describe --tags --abbrev=0)..HEAD --oneline').trim()
-                        sh (' ls -l ')
-                        println "Change: ${change}"
-                        deleteDir()
-                    }
-                }
-            }
-        }
+
 
         //     stage('show available date') {
         //   steps {
@@ -78,9 +64,9 @@ pipeline {
         // }
 
         stage('Create tag') {
-            when {
-                expression { change != '' }
-            }
+            // when {
+            //     expression { change != '' }
+            // }
             steps {
                 script {
                     echo 'Setting git safe.directory'
@@ -97,9 +83,9 @@ pipeline {
         }
 
         stage('Save tag') {
-            when {
-                expression { change != '' }
-            }
+            // when {
+            //     expression { change != '' }
+            // }
             steps {
                 script {
                     echo 'Setting git safe.directory'
@@ -139,6 +125,21 @@ pipeline {
                     //     sh (" git commit -am 'Updated version number to ${version}.${env.BUILD_ID}'")
                     //     sh (' git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags HEAD:main')
                     // }
+                }
+            }
+        }
+
+        stage('Check changes') {
+            steps {
+                script {
+                    dir ("changes"){
+                        sh (' ls -l ')
+                        cloneToLocation('https://github.com/girafrica/test1.git', 'github-app', 'main', '.')
+                        change = sh(returnStdout: true, script: 'git log $(git describe --tags --abbrev=0)..HEAD --oneline').trim()
+                        sh (' ls -l ')
+                        println "Change: ${change}"
+                        deleteDir()
+                    }
                 }
             }
         }
